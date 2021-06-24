793d064742373b2e80ef9576bce65671b507acb5
marcus bianchi <marcusbianchi92@hotmail.com>
2021-06-24 11:51:14 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+###############################################
+#------- > Exercise 1.1
+def valida_int(pergunta, min, max):
+    x = int(input(pergunta))
+    while ((x < min) or (x > max)):
+        x = int(input(pergunta))
+    return  x
+def fatorial(num):
+    fat = 1
+    if num == 0:
+        return fat
+    for i in range(1, num+1,1):
+        fat *= i
+    return fat
+
+#Programa principal
+x = valida_int('Digite um valor para calcular a fatorial: ', 0, 99999)
+print('{}! = {}'.format(x, fatorial(x)))
+help(fatorial)
